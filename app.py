e422fb7194254d1154c652ba9802c7700134e0b6
Sahil Chaudhary <sahil280114@gmail.com>
2022-08-23 18:34:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ from io import BytesIO
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
+    HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
     
     # this will substitute the default PNDM scheduler for K-LMS  
     lms = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear")
 
-    model = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", scheduler=lms, use_auth_token=True).to("cuda")
+    model = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", scheduler=lms, use_auth_token=HF_AUTH_TOKEN).to("cuda")
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
