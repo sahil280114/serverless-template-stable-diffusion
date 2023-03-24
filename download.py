7097e9e1378d2868adeb44754c2621df175cbc81
Sahil Chaudhary <sahil280114@gmail.com>
2023-03-24 23:10:23 +0100

    Update download.py

--- download.py
+++ download.py
@@ -9,17 +9,5 @@ def download_model():
     #Set auth token which is required to download stable diffusion model weights
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
 
-    lms = LMSDiscreteScheduler(
-        beta_start=0.00085, 
-        beta_end=0.012, 
-        beta_schedule="scaled_linear"
-    )
-
-    model = StableDiffusionPipeline.from_pretrained(
-        "CompVis/stable-diffusion-v1-4", 
-        scheduler=lms,
-        use_auth_token=HF_AUTH_TOKEN
-    )
-
 if __name__ == "__main__":
     download_model()
